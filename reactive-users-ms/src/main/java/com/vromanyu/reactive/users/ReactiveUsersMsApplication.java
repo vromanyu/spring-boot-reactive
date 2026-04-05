776bec18eb22f10a3e5c2dac8aeab640ae341574
vromanyu <vromanyu@outlook.com>
2026-04-05 16:58:40 +0300

    init reactive-users-ms

---
+++ reactive-users-ms/src/main/java/com/vromanyu/reactive/users/ReactiveUsersMsApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.reactive.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveUsersMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveUsersMsApplication.class, args);
+    }
+
+}
---
+++ reactive-users-ms/src/test/java/com/vromanyu/reactive/users/ReactiveUsersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vromanyu.reactive.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveUsersMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
